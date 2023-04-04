facd04619ef58b6b04a749828a948f90f653ed63
leaftime1 <112911703+leaftime1@users.noreply.github.com>
2023-04-04 09:38:47 -0300

    Add files via upload

---
+++ exercicio4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define num_steps 300000000
+
+int main() {
+    double step;
+    double pi, inicio, fim;
+    int t = omp_get_num_threads();
+    pi = 0;
+    inicio = omp_get_wtime();
+    step = 1.0/(double) num_steps;
+    #pragma omp parallel num_threads(4)
+    {
+        double x,soma=0.0;
+        int i, id, nthrds;
+        id = omp_get_thread_num();
+        nthrds = omp_get_num_threads();
+        for (i=id,soma=0.0;i< num_steps; i=i+nthrds){
+            x = (i + 0.5) * step;
+            soma = soma + 4.0 / (1.0 + x*x);
+        }
+        #pragma omp critical
+            pi+=soma*step;
+    }
+    fim = omp_get_wtime();
+    double t_paralelo = fim-inicio;
+    printf("Pi: %f\n",pi);
+    printf("Execucao paralela(s): %f\n",t_paralelo);
+    return 0;
+}
